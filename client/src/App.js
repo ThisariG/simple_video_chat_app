773d16a7f499f993b6817799e295a9f4d27a1a2f
Thisari Gunawardena <62197186+ThisariG@users.noreply.github.com>
2024-01-14 11:36:43 +0530

    Update the application name

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
       <div>
         <AppBar position="static" color="inherit" className={classes.appBar}>
           <Typography variant="h2" align="center">
-            Video Chat
+            EasyTalk
           </Typography>
         </AppBar>
         <VideoPlayer />
